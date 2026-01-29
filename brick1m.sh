dbc98cf29ac87abca398d79a03baa8e2c5e4a75f
RajasriAlapati <alapati.r@mobiusdtaas.ai>
2026-01-29 18:43:49 +0530

    Install Kaggle API if not already installed
    Add installation of Kaggle API using pip.

--- brick1m.sh
+++ brick1m.sh
@@ -18,6 +18,8 @@ PI_UPLOAD_URL="https://ig.gov-cloud.ai/pi-ingestion-service-dbaas/v2.0/jobs/uplo
 # VALIDATIONS
 ########################################
 
+python3 -m pip install --user kaggle
+
 if [ -z "$AUTH_TOKEN" ]; then
   echo "❌ AUTH_TOKEN is missing"
   exit 1
